2a869974f74ab10e16dee635e75bac38213a091a
YaoXingCoder <JiaZiChunQiu@163.com>
2024-09-04 10:58:21 +0800

    写时复制String实现

--- Basis/0.Project_Exercise/3_string_implement/String.cpp
+++ Basis/0.Project_Exercise/3_string_implement/String.cpp
@@ -3,7 +3,153 @@
     > Author: JiaZiChunQiu
     > Created Time: 2024年09月03日 星期二 17时55分41秒
     > Mail: JiaZiChunQiu@163.com
-    > Title:
+    > Title: CowString 写时复制实现
     > Content:
  ************************************************************************/
 
+#include "String.hh"
+#include <cstring>
+
+/* 分配空间 */
+char* CowString::malloc(const char* pstr) {
+    if ( pstr ) { return new char[std::strlen(pstr) + 1 + kRefcountLength]() + kRefcountLength; }
+    else { return new char[kRefcountLength + 1]() + kRefcountLength; }
+}
+
+/* 初始化计数器 */
+void CowString::initRefcount() {
+    /* *(static_cast<int*>(static_cast<void*>(_pstr - kRefcountLength))) = 1; */
+    *(reinterpret_cast<int*>(_pstr - kRefcountLength)) = 1;
+}
+/* 计数器+1 */
+void  CowString::increaseRefcount() {
+    ++(*(reinterpret_cast<int*>(_pstr - kRefcountLength)));
+}
+/* 计数器-1 */
+void CowString::decreaseRefcount() {
+    --(*(reinterpret_cast<int*>(_pstr - kRefcountLength)));
+}
+
+/* 获取计数器 */
+int CowString::use_count() const {
+    return *(reinterpret_cast<int*>(_pstr - kRefcountLength));
+}
+
+/* 销毁 */
+void CowString::release() {
+    decreaseRefcount();
+    if ( use_count() == 0 ) {
+        delete[] (_pstr - kRefcountLength);
+        std::cout << "release heap space" << std::endl;
+    }
+    _pstr = nullptr;
+}
+
+/* 获取字符长度 */
+std::size_t CowString::size() const {
+    return std::strlen(_pstr);
+}
+
+/* 获取指针 */
+char* CowString::c_str() {
+    return _pstr;
+}
+
+/* 默认无参 */
+CowString::CowString() :_pstr(malloc()) {
+    initRefcount();
+    std::cout << "CowString() 无参构造" << std::endl;
+}
+
+/* 有参构造 */
+CowString::CowString(const char* pstr) : _pstr(malloc(pstr)) {
+    initRefcount();
+    std::strcpy(_pstr, pstr);
+    std::cout << "CowString(const char* pstr) 有参构造" << std::endl;
+}
+
+/* 拷贝 */
+CowString::CowString(const CowString & other) : _pstr(other._pstr) {
+    increaseRefcount();
+    std::cout << "CowString(const CowString &) 拷贝" << std::endl;
+}
+
+/* 移动构造 */
+CowString::CowString(CowString&& other) :_pstr(other._pstr){
+    increaseRefcount();
+    std::cout << "CowString(CowString&&) 移动构造" << std::endl;
+}
+
+/* 析构 */
+CowString::~CowString() {
+    release();
+    std::cout << "~CowString() 析构" << std::endl;
+}
+
+/* 赋值重载 */
+CowString & CowString::operator=(const CowString & other) {
+    if ( this != &other ) {
+        release();
+        _pstr = other._pstr;
+        increaseRefcount();
+    }
+    return *this;
+}
+
+/* 赋值移动 */
+CowString & CowString::operator=( CowString && other ) {
+    if ( this != &other ) {
+        release();
+        _pstr = other._pstr;
+        increaseRefcount();
+    }
+    return *this;
+}
+
+/* 成员访问重载, 使用代理模式 */
+CowString::CharProxy CowString::operator[]( std::size_t idx ) {
+    return CharProxy(*this, idx);
+}
+
+/* 输出运算符重载 */
+std::ostream & operator<<(std::ostream & os, const CowString & obj) {
+    return os << obj._pstr;
+}
+
+/* ------------------- CharProxy ------------------------ */
+/* 有参构造 */
+CowString::CharProxy::CharProxy(CowString & self, std::size_t idx) :_self(self), _idx(idx) {
+    std::cout << "CharProxy() 有参" << std::endl;
+}
+
+/* 特殊赋值重载 */
+char CowString::CharProxy::operator=(char ch) {
+    if ( _idx < _self.size() ) {
+        if ( _self.use_count() > 1 ) {
+            _self.decreaseRefcount();
+            /* 深拷贝 */
+            char * tmp = new char[_self.size() + 1]();
+            std::strcpy(tmp, _self._pstr);
+            _self._pstr = tmp;
+            /* 更新引用计数 */
+            _self.initRefcount();
+        }
+        _self._pstr[_idx] = ch;
+        return _self._pstr[_idx];
+    } else { 
+        std::cout << "out of range" << std::endl;
+        static char tmp = '\0';
+        return tmp;
+    }
+}
+
+/* 赋值重载 */
+char CowString::CharProxy::operator=(const CowString::CharProxy & other) {
+    _self._pstr[_idx] = other._self._pstr[other._idx];
+    return _self._pstr[_idx];
+}
+
+/* 类型转换 */
+CowString::CharProxy::operator char() {
+    return _self._pstr[_idx];
+}
--- Basis/0.Project_Exercise/3_string_implement/main.cpp
+++ Basis/0.Project_Exercise/3_string_implement/main.cpp
@@ -7,27 +7,58 @@
     > Content:
  ************************************************************************/
 
-#include "SSOString.hh"
+/* #include "SSOString.hh" */
+#include "String.hh"
 
 /* =============== test =============== */
-void test0() {
-    /* SSOString s1; */ 
-    SSOString s2("hello");
-    std::cout << s2 << ", " << s2.size() << std::endl; 
-    SSOString s3("hello,world, it's time to happy.");
-    std::cout << s3 << ", " << s3.size() << std::endl; 
-    SSOString s4(SSOString("world"));
-    std::cout << s4 << ", " << s4.size() << std::endl;
-    SSOString s5(s2);
-    std::cout << s5 << ", " << s5.size() << std::endl;
-    /* s5 = s3; */
-    /* std::cout << s5 << ", " << s5.size() << std::endl; */
-    s5 = std::move(s3);
-    std::cout << s5 << ", " << s5.size() << std::endl;
-    std::cout << s3 << ", " << s3.size() << std::endl; 
+/* void test0() { */
+/*     /1* SSOString s1; *1/ */ 
+/*     SSOString s2("hello"); */
+/*     std::cout << s2 << ", " << s2.size() << std::endl; */ 
+/*     SSOString s3("hello,world, it's time to happy."); */
+/*     std::cout << s3 << ", " << s3.size() << std::endl; */ 
+/*     SSOString s4(SSOString("world")); */
+/*     std::cout << s4 << ", " << s4.size() << std::endl; */
+/*     SSOString s5(s2); */
+/*     std::cout << s5 << ", " << s5.size() << std::endl; */
+/*     /1* s5 = s3; *1/ */
+/*     /1* std::cout << s5 << ", " << s5.size() << std::endl; *1/ */
+/*     s5 = std::move(s3); */
+/*     std::cout << s5 << ", " << s5.size() << std::endl; */
+/*     std::cout << s3 << ", " << s3.size() << std::endl; */ 
+/* } */
+
+CowString func1() {
+    return CowString("test");
+}
+
+void test1() {
+    /* CowString cStr1; */
+    /* CowString cStr2("hello"); */
+    /* CowString cStr3(CowString("world")); */
+    /* CowString cStr4 = cStr2; */
+    /* cStr4 = std::move(cStr3); */
+    CowString cStr5 = CowString("hi");
+    cStr5 = func1();
+    /* std::cout << "cStr2 count : "  << cStr2.use_count() << std::endl; */
+    /* std::cout << "cStr3 count : "  << cStr3.use_count() << std::endl; */
+    std::cout << "cStr5 count : "  << cStr5.use_count() << std::endl;
+    /* std::cout << cStr2 << std::endl; */
+    /* std::cout << cStr3 << std::endl; */
+}
+
+/* 成员访问 */
+void test2() {
+    CowString str1("hello");
+    str1[3] = str1[1] = 'E';
+    char ch = str1[1];
+    std::cout << ch << std::endl;
+    std::cout << str1 << std::endl;
 }
 /* =============== main =============== */
 int main (int argc, char* argv[]) {
-    test0();
+    /* test0(); */
+    /* test1(); */
+    test2();
     return 0;
 }
