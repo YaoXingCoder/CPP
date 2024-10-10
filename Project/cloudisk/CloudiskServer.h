4c8645e8938bc82b7e3ffdd99b06105dc9d1d8e2
YaoXingCoder <JiaZiChunQiu@163.com>
2024-10-10 22:44:18 +0800

    cloudisk_aliyun_oss

--- Project/cloudisk/CloudiskServer.h
+++ Project/cloudisk/CloudiskServer.h
@@ -1,34 +1,39 @@
-#ifndef __WD_CloudiskServer_HPP__ 
-#define __WD_CloudiskServer_HPP__ 
+#ifndef __WD_CloudiskServer_HPP__
+#define __WD_CloudiskServer_HPP__
 
-#include <workflow/WFFacilities.h>
 #include <wfrest/HttpServer.h>
+#include <workflow/WFFacilities.h>
 
 #define RETRY_MAX 1
-#define LOCAL_NGINX_URL "localhost:6379"
+#define REDIRECT_MAX 5
+#define LOCAL_NGINX_URL "localhost:8080"
 
-class CloudiskServer
-{
-public:
-    CloudiskServer(int cnt) 
-    : _waitGroup(cnt)
-    {}
+/* OSS 相关 */
+#define EndPoint "xxxxx.com"  // 外网访问地域节点
+#define AccessKeyID "xxxxxx"
+#define ACCESSS_KEY_SECRET "XXXXXX"
+#define BUCKET "XXXXX"            // 存储空间名称
+#define CloudFilePath "newFile/"  // 云端文件路径
+
+class CloudiskServer {
+   public:
+    CloudiskServer(int cnt) : _waitGroup(cnt) {}
     ~CloudiskServer() {}
     void start(unsigned short port);
     void loadModules();
-private:
-    //模块化的思维方式写代码
-    void loadStaticResourceModule();
-    void loadUserRegisterModule();
-    void loadUserLoginModule();
-    void loadUserInfoModule();
-    void loadFileQueryModule();
-    void loadFileUploadModule();
-    void loadFileDownloadModule();
-private:
-    WFFacilities::WaitGroup _waitGroup;
+
+   private:
+    // 模块化的思维方式写代码
+    void loadStaticResourceModule();  // 静态资源
+    void loadUserRegisterModule();    // 注册
+    void loadUserLoginModule();       // 登录
+    void loadUserInfoModule();        // 用户信息
+    void loadFileQueryModule();       // 文件查询
+    void loadFileUploadModule();      // 文件上传
+    void loadFileDownloadModule();    // 文件下载
+   private:
+    WFFacilities::WaitGroup _waitGroup;  // 时间等待
     wfrest::HttpServer _httpserver;
 };
 
 #endif
-
